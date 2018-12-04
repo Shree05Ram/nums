16a8eebd82f770490db433145eb4eba4b414ceb4
Shree05Ram <shreeram@aikaan.io>
2018-12-04 09:43:48 +0530

    third

--- num.js
+++ num.js
@@ -1,12 +1,21 @@
 let lb='<br>'
-for(let i=1;i<=100;i++)
+for(let i=1;i<=50;i++)
 {
     if(i%3==0 && i%5==0)
+    {
+        for(let j=0;j<((i/3)-(i/5));j++)
         document.write('FizzBuzz');
+    }
     else if(i%3==0)
+    {
+        for(j=0;j<(i/3);j++)
         document.write('Fizz');
+    }
     else if(i%5==0)
+    {
+        for(j=0;j<(i/5);j++)
         document.write('Buzz');
+    }
     else
         document.write(i);
     document.write(lb);
